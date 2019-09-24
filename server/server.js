c9fb39b007d9f89b885943a65e8daddbcd767934
Sarwan Nizamani <Sarwan Nizamani>
2019-09-25 00:33:49 +0500

    Add SomeCode to server.js file

--- server/server.js
+++ server/server.js
@@ -2,6 +2,8 @@ const express=require('express');
 const app=express();
 const path=require('path');
 const bodyParser=require('body-parser');
+const request=require('request');
+
 
 app.use(bodyParser.urlencoded({extended:true}));
 
@@ -12,8 +14,13 @@ Response.sendFile(path.normalize(__dirname+"/../src/index.html"));
 
 });
 
-app.post("/",(Request,Response)=>{
-    console.log(Request.body.crypto);
+app.post("/",(req,res)=>{
+    
+    request("https://apiv2.bitcoinaverage.com/indices/global/ticker/BTCUSD",(error,Response,body)=>{
+
+console.log(body);
+    });
+
 });
 
 app.listen(3000,(Request,Response)=>{
