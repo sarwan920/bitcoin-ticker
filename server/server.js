10578b816bcfe8b2aa7cdaf7cd90da992d71457a
Sarwan Nizamani <Sarwan Nizamani>
2019-09-25 01:42:29 +0500

    add some code to index.html and server.js

--- server/server.js
+++ server/server.js
@@ -18,14 +18,27 @@ app.post("/",(req,res)=>{
 
     var crypto=req.body.crypto;
     var fiat=req.body.fiat;
-    var baseURL="https://apiv2.bitcoinaverage.com/indices/global/ticker/"
-    var finalURL=baseURL+crypto+fiat;
+    var amount=req.body.amount;
+   
+    var options={
+        url:"https://apiv2.bitcoinaverage.com/convert/global",
+        method: "GET",
+        qs:{
+            from:crypto,
+            to:fiat,
+            amount:amount
+        }
+    };
     
-    request(finalURL,(error,Response,body)=>{
+    request(options,(error,Response,body)=>{
 
     var data=JSON.parse(body);
-    var price=data.last;
-   res.send("price of "+crypto+" in  " +fiat+" is "+price);
+    var price=data.price;
+    var currentDate=data.time;
+
+    res.write("<p>The Current time and Date is"+ currentDate+"</p>")
+   res.write("<h1>"+amount+" "+ crypto +" is worth "+price+"</h2>");
+   res.send();
 
     });
 
